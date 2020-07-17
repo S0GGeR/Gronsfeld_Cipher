1c3efcede1ab26e65466bdbc3b31649a1b4acccb
SoGGeR-laptop <sogger.tur@gmail.com>
2020-07-17 21:07:41 +0300

    Release

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
 
 from PySide2 import QtWidgets
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
-                            QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-                           QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
-                           QPixmap, QRadialGradient)
+from PySide2.QtCore import QRect
+
 from PySide2.QtWidgets import *
 
 from interface import Ui_Dialog
@@ -13,17 +10,22 @@ from interface import Ui_Dialog
 
 def gronsfeld_code(message, key, state, alphabet):
     key *= len(message) // len(key) + 1
-    message = message.lower()
     return ''.join([ alphabet[ alphabet.index(j) + int(key[ i ]) * state ] for i, j in enumerate(message) ])
+
+
+def caesar_code(message, key, state, alphabet):
+    key = int(key)
+    return ''.join([ alphabet[ alphabet.index(j) + int(key) * state ] for i, j in enumerate(message) ])
 
 
 class Application(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.method = 0
         self.alphabet = ''
         self.state = 0
-        self.key = ''
+        self.key = '0'
         self.message = ''
         self.ui = Ui_Dialog()
         self.initUI()
@@ -36,9 +38,9 @@ class Application(QWidget):
         # Hook logic
         self.ui.buttonGroup.buttonClicked.connect(self.alphabet_init)
         self.ui.buttonGroup_2.buttonClicked.connect(self.state_init)
+        self.ui.buttonGroup_3.buttonClicked.connect(self.method_init)
         self.ui.message_field.editingFinished.connect(self.message_init)
         self.ui.key_field.editingFinished.connect(self.key_init)
-
         self.ui.pushButton.clicked.connect(self.cipher_init)
 
     def alphabet_init(self, check_box):
@@ -49,7 +51,7 @@ class Application(QWidget):
         elif check_box.isChecked() and check_box.objectName() == 'German':
             self.alphabet = 'aäbcdefghijklmnoöpqrsßtuüvwxyz' * 2
         else:
-            self.alphabet = self.ui.alphabet_field.text()
+            self.alphabet = self.ui.alphabet_field.text().replace(' ', '')
 
     def state_init(self, check_box):
         if check_box.isChecked() and check_box.objectName() == 'encryption':
@@ -58,14 +60,48 @@ class Application(QWidget):
             self.state = -1
 
     def key_init(self):
-        self.key = self.ui.key_field.text()
+        self.key = self.ui.key_field.text().replace(' ', '')
+        try:
+            int(self.key)
+        except:
+            self.ui.error_label_3.setText(
+                'Пожалуйста, используйте в поле ключа только арабские цифры в десятичной системе счисления!')
 
     def message_init(self):
-        self.message = self.ui.message_field.text()
+        self.message = self.ui.message_field.text().lower().replace(' ', '')
+        # Checking for letters in a message that are not in the alphabet
+        j = 0
+        for letter in self.message:
+            for i in range(0, len(self.alphabet)):
+                if letter == self.alphabet[ i ]:
+                    j += 1
+                    break
+        if j != len(self.message):
+            self.ui.error_label_2.setText('Пожалуйста, используйте символы только из алфавита!')
+            # self.ui.error_label.setGeometry(QRect(200, 290, 570, 20))
+            self.message = ''
+            self.ui.message_field.setText('')
+        elif j == len(self.message) and self.ui.error_label.text() == (
+                'Пожалуйста, используйте символы только из алфавита!'):
+            self.ui.error_label.setText(' ')
 
     def cipher_init(self):
-        result = gronsfeld_code(self.message, self.key, self.state, self.alphabet)
-        self.ui.textBrowser.setText(result)
+        if self.alphabet != '' and self.state != 0 and self.key != '0' and self.message != '' and self.method != 0:
+            self.ui.error_label.setText('')
+            if self.method == 1:
+                result = caesar_code(self.message, self.key, self.state, self.alphabet)
+            elif self.method == 2:
+                result = gronsfeld_code(self.message, self.key, self.state, self.alphabet)
+            self.ui.textBrowser.setText(result)
+
+        else:
+            self.ui.error_label.setText('Пожалуйста, заполните все поля!')
+
+    def method_init(self, check_box):
+        if check_box.isChecked() and check_box.objectName() == 'caesar':
+            self.method = 1
+        elif check_box.isChecked() and check_box.objectName() == 'gronsfeld':
+            self.method = 2
 
 
 if __name__ == "__main__":
